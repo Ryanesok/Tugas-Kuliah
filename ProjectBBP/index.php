22a339d1e5a5822aa80e67272d0911eb22e3608f
Ryanesok <153131627+Ryanesok@users.noreply.github.com>
2024-12-01 23:30:19 +0700

    Update index.php
    tambahan table dari database dan css

--- ProjectBBP/index.php
+++ ProjectBBP/index.php
@@ -1,4 +1,6 @@
 <?php
+    require 'functions.php';
+    $data = query("SELECT * FROM resiko");
     if(isset($_POST["login"])) {
         //cek username dan password
         if($_POST["username"] == "admin" && $_POST["password"] == "123") {
@@ -18,37 +20,53 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aplikasi Risiko Mitigasi</title>
-    <style>
-        h1{
-            text-align: center;
-            background-color: aquamarine;
-            border: 50px;
-            color: black;
-        }
-        a {
-            background-color: antiquewhite;
-            color: black;
-        }
-    </style>
+    <link rel="stylesheet" href="resources/style.css">
 </head>
 <body>
     <h1>Aplikasi Manajemen Risiko</h1>
-    <form action="" method="post">
-        <ul>
-            <li>
-                <label for="username">Username: </label>
-                <input type="text" name="username" id="username">
-            </li>
-            <li>
-                <label for="password">Password: </label>
-                <input type="password" name="password" id="password">
-            </li>
-            <li>
-                <button type="submit" name="login">login</button>
-            </li>
-            
-        </ul>
-    </form>
-    
+    <div class="form-container">
+            <form action="" method="post">
+            <div class="inputs">
+                <label for="username">Username:</label>
+                <input type="text" name="username" id="username" placeholder="Masukkan username">
+
+                <label for="password">Password:</label>
+                <input type="password" name="password" id="password" placeholder="Masukkan password">
+            </div>
+
+            <!-- Login Button -->
+            <div class="login-button">
+                <button type="submit" name="login">Login</button>
+            </div>
+            </form>
+        </div>
+        <h2>Tabel Manajemen Resiko</h2>
+    <section>
+        <table>
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Resiko</th>
+                    <th>Divisi</th>
+                    <th>Tingkat Resiko</th>
+                    <th>Penyebab Resiko</th>
+                    <th>Sumber Resiko</th>
+                    <th>Mitigasi</th>
+                    <th>Solusi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php $i = 1?>
+                <?php foreach($data as $dat):?>
+                    <td><?= $i; ?></td>
+                    <td><?= $dat["resiko"]?></td>
+                    <td><?= $dat["divisi"]?></td>
+                    <td><?= $dat["tingkat"]?></td>
+                    <td><?= $dat["penyebab"]?></td>
+                    <td><?= $dat["sumber"]?></td>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </section>
 </body>
 </html>
